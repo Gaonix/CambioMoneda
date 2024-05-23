6ec15cca9b462b05700d420b1948ee3af6adeae4
fernando gaona <fernandocpagna@gmail.com>
2024-05-22 23:43:49 -0500

    arreglado

--- src/co/edu/unbosque/controller/RamificaPoda.java
+++ src/co/edu/unbosque/controller/RamificaPoda.java
@@ -36,37 +36,44 @@ public class RamificaPoda {
 		if(var!=0) {
 			throw new RuntimeErrorException(null,"No existen combinaciones para ese cambio" );
 		}
-		Cambio(valor,0, 0);
+		 int[] cantidades = new int[5];
+		Cambio(valor,0, 0,cantidades);
 		}catch(Exception e){
 			vis.imprimirConSalto(e.getMessage());
-		run();	
+			run();	
 		}
 
 		
 	}
 
-	private void Cambio(int valor, int index, int can) {
-		if(valor==0) {
-			if(can<cantidadMonedas) {
-				cantidadMonedas=can;
-				mejorSolucion.clear();
-				mejorSolucion.addAll(solucionA);
-			}
-			return;
-		}
-		if(valor<0||can>=cantidadMonedas) {
-			return;
-		}
-		for (int i=index;i<caja.size();i++) {
-			Moneda m=caja.get(i);
-			if(m.getValor()>valor) {
-				continue;
-			}
-			solucionA.add(m);
-			Cambio( valor-m.getValor(),i,can+1);
-			solucionA.remove(solucionA.size()-1);
-		}
+	private void Cambio(int valor, int index, int can, int[] cantidades) {
+	    if (valor == 0) {
+	        if (can < cantidadMonedas) {
+	            cantidadMonedas = can;
+	            mejorSolucion.clear();
+	            mejorSolucion.addAll(solucionA);
+	        }
+	        return;
+	    }
+	    if (valor < 0 || can >= cantidadMonedas) {
+	        return;
+	    }
+	    for (int i = index; i < caja.size(); i++) {
+	        Moneda m = caja.get(i);
+	        if (m.getValor() > valor) {
+	            continue;
+	        }
+	        if (cantidades[i] >= m.getCantidad()) {
+	            continue; 
+	        }
+	        cantidades[i]++;
+	        solucionA.add(m);
+	        Cambio(valor - m.getValor(), i, (can + 1), cantidades);
+	        solucionA.remove(solucionA.size() - 1);
+	        cantidades[i]--;
+	    }
 	}
+
 
 	private void cargarMonedas() {
 		caja.add(new Moneda("1000 pesos", 10, 1000));
@@ -78,12 +85,41 @@ public class RamificaPoda {
 	}
 
 	public void imprimirSolucion() {
-		vis.imprimirConSalto("La mejor solucion es: ");
+		
 	if (mejorSolucion.isEmpty()) {
 		vis.imprimirConSalto("Las monedas no son suficientes:");
 	}
+	vis.imprimirConSalto("La mejor solucion es: "+ mejorSolucion.size());
+	int c1000=0;
+	int c500=0;
+	int c200=0;
+	int c100=0;
+	int c50=0;
 	for (Moneda i: mejorSolucion) {
-		vis.imprimirConSalto(i.getNombre());
+		
+		if(i.getValor()==1000) {
+			c1000++;
+		}if(i.getValor()==500) {
+			c500++;
+		}if(i.getValor()==200) {
+			c200++;
+		}if(i.getValor()==100) {
+			c100++;
+		}if(i.getValor()==50) {
+			c50++;
+		}
+		
+	}
+	if(c1000!=0) {
+		vis.imprimirConSalto("Monedas de 1000 :"+c1000);
+	}if(c500!=0) {
+		vis.imprimirConSalto("Monedas de 500 :"+c500);
+	}if(c200!=0) {
+		vis.imprimirConSalto("Monedas de 200 :"+c200);
+	}if(c100!=0) {
+		vis.imprimirConSalto("Monedas de 100 :"+c100);
+	}if(c50!=0) {
+		vis.imprimirConSalto("Monedas de 50 :"+c50);
 	}
 	
 	
